bc1f2b2a4734bc6f0486880e33c024b1e0857ea4
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-03-28 18:51:50 -0400

    refactored and added more tests to check for all the sightings cards

--- cypress/integration/allsightings-test.js
+++ cypress/integration/allsightings-test.js
@@ -8,20 +8,29 @@ describe('All sightings page cypress test', () => {
     expect(true).to.equal(true)
   });
 
-  it('Should start on the home page and click sighting buttom', () => {
+  it('Should start on the home page and click sighting button', () => {
     cy.get('button').click()
   })
 
+  it('Should display the Seach by Cryptids button and the search by zipcode', ()=> {
+    cy.get('button').click()
+    cy.get('.cryptid-button')
+    cy.get('.zipcode-input')
+  })
+
+  it('Should display all the sightings of the cryptids', () => {
+    cy.get('button').click()
+    cy.get('.all-sightings-container')
+  })
+
   it('Should click on logo and move to home page', () => {
+    cy.get('button').click()
     cy.get('.logo').click()
-    cy.get('#root > :nth-child(3)')
-      .contains('Home Page')
   })
 
   it('Should click on ok, Cryptid and move to home page', () => {
-    cy.get('.logo').click()
-    cy.get('#root > :nth-child(3)')
-      .contains('Home Page')
+    cy.get('button').click()
+    cy.get('.logo-text').click()
   })
 
 });
