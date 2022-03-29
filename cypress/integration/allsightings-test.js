2bba9a47822e7663d2dd17bc0578930365af55b4
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-03-29 11:19:02 -0400

    changed local host to 3000

--- cypress/integration/allsightings-test.js
+++ cypress/integration/allsightings-test.js
@@ -1,7 +1,7 @@
 describe('All sightings page cypress test', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:3001')
+    cy.visit('http://localhost:3000')
   })
   it('Should confirm that true is equal to true', () => {
     expect(true).to.equal(true)
--- cypress/integration/home-test.js
+++ cypress/integration/home-test.js
@@ -1,7 +1,7 @@
 describe('Home page cypress test', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:3001')
+    cy.visit('http://localhost:3000')
   })
   it('Should confirm that true is equal to true', () => {
     expect(true).to.equal(true)
