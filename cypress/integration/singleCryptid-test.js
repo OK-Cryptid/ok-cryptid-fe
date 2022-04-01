7d550969ee3e01cbb9c3410d0b07d5d1ecae80bc
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-03-31 21:40:01 -0400

    Made tests that check the contents of the single cryptids

--- cypress/integration/singleCryptid-test.js
+++ cypress/integration/singleCryptid-test.js
@@ -3,17 +3,11 @@ describe('All sightings page cypress test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
-<<<<<<< Updated upstream
-
-  it('Should start on the home page and click on a cryptid', () => {
-    cy.get('.card-img').eq(0).click()
-=======
   
 
   it('Should start on the home page and click on a cryptid', () => {
     cy.get('.home-message').contains('Ok Cryptid')
     cy.get('\#\\32').click()
->>>>>>> Stashed changes
   })
 
   it('Should view the cryptids information page', () => {
@@ -22,7 +16,7 @@ describe('All sightings page cypress test', () => {
     cy.get('.cryptid-details')
     cy.get('.cryptid-name').contains('Bigfoot')
     cy.get('.cryptid-danger').contains('Danger Level:')
-    cy.get('.gold-container').contains('6/10')
+    cy.get('.gold-container').contains('7/10')
     cy.get('.cryptid-description').contains('Description:')
     cy.get('.cryptid-button-sightings')
   })
