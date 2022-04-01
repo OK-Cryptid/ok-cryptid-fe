b632f89c88747970ea0b71fada4b627934e99319
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-03-31 21:41:01 -0400

    Created tests to check to see ift all the sighting cards show, along with the contents of the card, and tested for the filtering of the cryptids

--- cypress/integration/allsightings-test.js
+++ cypress/integration/allsightings-test.js
@@ -3,11 +3,7 @@ describe('All sightings page cypress test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
-<<<<<<< Updated upstream
-
-=======
   
->>>>>>> Stashed changes
   it('Should start on the home page and click sighting button', () => {
     cy.get('.logo')
     cy.get('.home-message').contains('Ok Cryptid')
@@ -25,6 +21,24 @@ describe('All sightings page cypress test', () => {
   it('Should display all the sightings of the cryptids', () => {
     cy.get('.sightings-button').click()
     cy.get('.all-sightings-container')
+  })
+
+  it('Should click on a sighting of a random cryptoid, and view itss information', () => {
+    cy.get('.sightings-button').click()
+    cy.get('\#\\32').click()
+    cy.get('.sighting-photo')
+    cy.get('.sighting-title').contains('Section A-17 Prototype Labs')
+    cy.get('.sighting-location').contains('Location:')
+    cy.get('#sightingDescription')
+  })
+
+  it('Should filter by searched cryptid and see all the sightings of those cryptoids and move back to all the sightings', () => {
+    cy.get('.sightings-button').click()
+    cy.get('.cryptid-button').click()
+    cy.get('#Skinwalker').click()
+    cy.get('.all-sightings-container')
+    cy.get('.cryptid-button').click()
+    cy.get('.dropdown-container > :nth-child(1)').click()
   })
 
   it('Should click on logo and move to home page', () => {
