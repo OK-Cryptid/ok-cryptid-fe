55db4ec2c472391b1a97f5a6aafa0c39f9db3da2
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-03-31 21:39:19 -0400

    Refactored the tests that I did with Grace and added a test for it to go back home

--- cypress/integration/home-test.js
+++ cypress/integration/home-test.js
@@ -3,11 +3,7 @@ describe('Home page cypress test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
-<<<<<<< Updated upstream
-
-=======
   
->>>>>>> Stashed changes
   it('Should render elements to the page', () => {
     cy.get('.logo')
     cy.get('.home-message')
@@ -18,6 +14,10 @@ describe('Home page cypress test', () => {
   it('Should click on button and move to sightings page', () => {
     cy.get('.sightings-button').click()
     cy.get('.sightings-text').contains('Sightings')
+  })
+  it('Should click on the logo and go back to the home page', () => {
+    cy.get('.logo-text').click()
+    cy.get('.home-message').contains('Ok Cryptid')
   })
 
 });
